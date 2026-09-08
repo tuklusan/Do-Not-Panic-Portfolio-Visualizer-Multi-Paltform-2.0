69daca63664ff0c5d89e0c5d8bd846cbce6006be
Supratim Sanyal <sanyal.duckdns.org.openvms.vax@gmail.com>
2026-09-08 11:18:58 -0400

    Block sshpass across Mac harness transfers

--- build/Invoke-LocalLabSoakCycle.ps1
+++ build/Invoke-LocalLabSoakCycle.ps1
@@ -159,6 +159,9 @@ function Copy-RemoteTree {
         [Parameter(Mandatory = $true)][string]$LocalPath,
         [Parameter()][int]$Timeout = 900
     )
+    if ($HostName -eq '192.168.4.77' -or $User -eq 'rumtuk') {
+        throw 'MAC_SSH_ACCESS_FAIL=sshpass_forbidden; use native interactive ssh with a PTY and operator-entered password.'
+    }
     $psi = [Diagnostics.ProcessStartInfo]::new()
     $psi.Environment.Remove('SSHPASS')
     $psi.Environment['SSHPASS'] = $Secret
@@ -192,6 +195,9 @@ function Copy-LocalTree {
         [Parameter(Mandatory = $true)][string]$RemotePath,
         [Parameter()][int]$Timeout = 900
     )
+    if ($HostName -eq '192.168.4.77' -or $User -eq 'rumtuk') {
+        throw 'MAC_SSH_ACCESS_FAIL=sshpass_forbidden; use native interactive ssh with a PTY and operator-entered password.'
+    }
     $psi = [Diagnostics.ProcessStartInfo]::new()
     $psi.Environment.Remove('SSHPASS')
     $psi.Environment['SSHPASS'] = $Secret
